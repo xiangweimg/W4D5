d84bed75a7690ff29d6520c83fa247cfc47dcd8d
Eugene Kofman <eugenekofman@gmail.com>
2022-08-12 15:20:06 -0400

    largest sum On

--- execution_time.rb
+++ execution_time.rb
@@ -1,4 +1,4 @@
-def my_min(list)
+def my_min(list) #n ^ 2
     result = list.first
     (0...list.length).each do |idx1|
         
@@ -14,7 +14,7 @@ def my_min(list)
     result
 end
 
-# def my_min(list)
+# def my_min(list) n 
 #     result = list.first
 #     i = 0 
 #     while i < list.length 
@@ -26,48 +26,69 @@ end
 #     result
 # end
 
-list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-p  my_min(list)  # =>  -5
+# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# p  my_min(list)  # =>  -5
+
+
+# def largest_sub_sum(list) # n^2 + 2n
+#     subs = []
+#     list.each_with_index do |ele1, i1|
+#         list.each_with_index do |ele2, i2|
+#             subs << list[i1..i2] 
+#         end
+#     end
+#     subs.reject!{|sub| sub.empty? }
+#     max_sub = subs.inject do |max, sub| 
+#         if sub.sum > max.sum
+#             sub
+#         else
+#             max
+#         end
+#     end
+#     max_sub.sum
+# end
 
 
 def largest_sub_sum(list)
-    subs = []
-    list.each_with_index do |ele1, i1|
-        list.each_with_index do |ele2, i2|
-            subs << list[i1..i2] 
+    max = list.first
+    current = list.first
+    list.drop(1).each do |ele|
+        # max += ele 
+        current = current + ele
+        if current < 0 && max > 0
+            current = 0
+        elsif  max < 0
+            max = ele if ele > max
+            
+        elsif current > max 
+            max = current
         end
+         
     end
-    subs.reject!{|sub| sub.empty? }
-    max_sub = subs.inject do |max, sub| 
-        if sub.sum > max.sum
-            sub
-        else
-            max
-        end
-    end
-    max_sub.sum
+
+    max
 end
 
-# def all_subs(list)
-#     return [list] if list.length <= 1
-#     first =[ list.first ]
-#     rest = list.drop(1)
-#     first + all_subs(rest)
-# end
-# def all_subs(list)
-#     i = 0
-#     subs = []
-#     while i < list.length 
-#         j = 0
-#         while j < list.length 
-#             subs << list[i..j]
-#             j += 1
-#         end
-#         i += 1
+
+
+# def largest_sub_sum(list)
+#     max_sum = list.sum 
+#     while list.length > 0 
+#         list.shift
+#         max_sum = list.sum if list.sum > max_sum
 #     end
-#     subs
+#     while list.length > 0 
+#         list.pop
+#         max_sum = list.sum if list.sum > max_sum 
+#     end
+#     max_sum           
 # end
+
+
 list = [5, 3, -7]
 p largest_sub_sum(list) # => 8
 list2 = [2, 3, -6, 7, -6, 7]
 p largest_sub_sum(list2) # => 8 (from [7, -6, 7])
+
+list = [-5, -1, -3]
+p largest_sub_sum(list) # => -1 (from [-1])
